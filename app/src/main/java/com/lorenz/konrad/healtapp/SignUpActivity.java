d153d402cef54800fc33cbc00d076027ed5aa598
Camilo Martínez <camc1501@gmail.com>
2017-09-11 11:58:11 -0500

    Added prototype of Loggin activity and part of Sign activity added prototype of loggin activity and part of sign activity without function, in sign activity are missing the part of clinic history and familiar history, logo profile open the activity camera.

---
+++ app/src/main/java/com/lorenz/konrad/healtapp/SignUpActivity.java
@@ -0,0 +1,31 @@
+package com.lorenz.konrad.healtapp;
+
+import android.content.Intent;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+
+        ImageView image = (ImageView) findViewById(R.id.sign_up_image);
+
+        image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+                startActivity(intent);
+            }
+        });
+
+
+    }
+}
